89bda02fa50b9dca0c480eeace98f4b7af05d837
Fernando Santos <fern@ndo.io>
2023-08-11 20:55:57 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,18 +12,6 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  webpack: (config, { nextRuntime }) => {
-    if (typeof nextRuntime === 'undefined') {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      }
-    }
-
-    return config
-  },
 }
 
 export default withSearch(
